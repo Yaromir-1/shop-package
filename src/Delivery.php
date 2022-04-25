c6162889249ee638188666bbeeceb249d62c545a
Yaromir <yya.uusatyuk@gmail.com>
2022-04-25 16:38:30 +0300

    added config geo selection

--- src/Delivery.php
+++ src/Delivery.php
@@ -2,6 +2,7 @@
 
 namespace Yaromir\ShopPackage;
 
+use Carbon\Exceptions\InvalidFormatException;
 use KMLaravel\GeographicalCalculator\Facade\GeoFacade;
 
 class Delivery
@@ -13,7 +14,23 @@ class Delivery
         $this->result = 0;
     }
 
-    public function calculateDeliveryPrice(float $latitude1, float $longitude1, float $latitude2, float $longitude2, float $pricePerHour)
+    private function calculateDeliveryPrice(float $latitude1, float $longitude1, float $latitude2, float $longitude2, float $pricePerHour)
+    {
+        $selectedService = config('shoppackage.selected_geo_service');
+        switch ($selectedService) {
+            case "mathematical":
+                $this->kmlaravelDeliveryPrice($latitude1,$longitude1, $latitude2, $longitude2, $pricePerHour);
+                break;
+            case "kmlaravel":
+                $this->mathDeliveryPrice($latitude1,$longitude1, $latitude2, $longitude2, $pricePerHour);
+                break;
+            default:
+                throw new InvalidFormatException('error');
+        }
+        return $this;
+    }
+
+    private function kmlaravelDeliveryPrice(float $latitude1, float $longitude1, float $latitude2, float $longitude2, float $pricePerHour)
     {
         $this->result = $pricePerHour * GeoFacade::setPoint([$latitude1, $longitude1])->setOptions(['units' => ['km']])
             ->setPoint([$latitude2, $longitude2])->getDistance()['1-2']['km'];
@@ -21,7 +38,7 @@ class Delivery
         return $this;
     }
 
-    public function calculateDeliveryPriceMath(float $latitude1, float $longitude1, float $latitude2, float $longitude2, float $pricePerHour)
+    private function mathDeliveryPrice(float $latitude1, float $longitude1, float $latitude2, float $longitude2, float $pricePerHour)
     {
         $theta = $longitude1 - $longitude2;
         $distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2))) + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta)));
